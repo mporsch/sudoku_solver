b46a7eeb7aea0f0a52b1a9f3d5686939f9cbd236
Marco Porsch <marco.porsch@posteo.de>
2024-06-01 12:21:25 +0200

    parse command line arguments using lexy

---
+++ args.h
@@ -0,0 +1,121 @@
+#pragma once
+
+#include "lexy/action/parse.hpp"
+#include "lexy/callback.hpp"
+#include "lexy/dsl.hpp"
+#include "lexy/input/string_input.hpp"
+#include "lexy_ext/report_error.hpp"
+
+#include <numeric>
+#include <optional>
+#include <iostream>
+#include <string>
+
+struct Args
+{
+  struct Template
+  {
+    using value_type = size_t;
+
+    value_type width;
+    value_type height;
+
+    constexpr Template(lexy::nullopt)
+      : width(3)
+      , height(3)
+    {
+    }
+
+    constexpr Template(value_type width, lexy::nullopt)
+      : width(width)
+      , height(width)
+    {
+    }
+
+    constexpr Template(value_type width, value_type height)
+      : width(width)
+      , height(height)
+    {
+    }
+  };
+
+  std::optional<bool> help;
+  std::optional<Template> templ;
+};
+
+namespace arg_parse_detail {
+
+#define ARG_HELP "--help"
+#define ARG_TEMPLATE "--template"
+
+namespace dsl = lexy::dsl;
+
+struct production
+{
+  static constexpr auto sep = dsl::newline;
+
+  struct help
+  {
+    static constexpr auto rule = dsl::while_(sep);
+
+    static constexpr auto value = lexy::constant(true);
+  };
+
+  struct template_
+  {
+    static constexpr auto rule = [] {
+      auto dim = dsl::integer<Args::Template::value_type>;
+      auto width_and_maybe_height = dim + dsl::opt(dsl::lit_c<'x'> >> dim);
+      return dsl::opt(dsl::lit_c<'='> >> width_and_maybe_height) + dsl::while_(sep);
+    }();
+
+    static constexpr auto value = lexy::construct<Args::Template>;
+  };
+
+  static constexpr auto rule = [] {
+    auto make_arg = [](auto name, auto rule) {
+      return name >> rule;
+    };
+
+    auto arg_help = make_arg(LEXY_LIT(ARG_HELP), LEXY_MEM(help) = dsl::p<help>);
+    auto arg_template = make_arg(LEXY_LIT(ARG_TEMPLATE), LEXY_MEM(templ) = dsl::p<template_>);
+
+    return dsl::partial_combination(arg_help, arg_template) + dsl::eof;
+  }();
+
+  static constexpr auto value = lexy::as_aggregate<Args>;
+};
+
+} // namespace arg_parse_detail
+
+struct Usage
+{
+  const char* name;
+};
+
+std::ostream& operator<<(std::ostream& os, const Usage& u)
+{
+  os << "Usage: " << u.name << "\n"
+    << "  [" ARG_HELP "]              show this help\n"
+    << "  ["  ARG_TEMPLATE  "[=W[xH]]]  print a template of dimensions width x height blocks to use for input\n";
+  return os;
+}
+
+Args ParseArgs(int argc, char** argv)
+{
+  using namespace arg_parse_detail;
+
+  auto concat = [](std::string str, const char* arg) -> std::string {
+    return (std::move(str) + arg) + '\n';
+  };
+  auto str = std::accumulate(argv + 1, argv + argc, std::string(), concat);
+
+  auto input = lexy::string_input(str);
+  if(auto parsed = lexy::parse<production>(input, lexy_ext::report_error); parsed.has_value()) {
+    return parsed.value();
+  }
+  return Args{};
+}
+
+#undef ARG_HELP
+#undef ARG_TEMPLATE
--- sudoku_solver.cpp
+++ sudoku_solver.cpp
@@ -1,29 +1,15 @@
+#include "args.h"
 #include "grid.h"
 #include "grid_parse.h"
 #include "grid_print.h"
 
 #include <cstdlib>
-#include <limits>
 #include <sstream>
-#include <string>
-#include <string_view>
 
-template<typename T>
-T ParseValue(std::string const &arg)
+int PrintUsage(std::ostream& os, const char* name)
 {
-  long long num;
-  if(!(std::istringstream(arg) >> num) ||
-     (num < std::numeric_limits<T>::min()) ||
-     (num > std::numeric_limits<T>::max())) {
-    throw std::invalid_argument("Invalid number '" + arg + "'");
-  }
-  return static_cast<T>(num);
-}
-
-template<typename T>
-T ParseValue(std::string_view arg)
-{
-  return ParseValue<T>(std::string(arg));
+  os << Usage{name} << std::endl;
+  return EXIT_SUCCESS;
 }
 
 int PrintTemplate(unsigned w, unsigned h)
@@ -32,44 +18,15 @@ int PrintTemplate(unsigned w, unsigned h)
   return EXIT_SUCCESS;
 }
 
-int PrintTemplate(unsigned w)
-{
-  return PrintTemplate(w, w);
-}
-
-constexpr auto argHelp = std::string_view("--help");
-constexpr auto argTemplate = std::string_view("--template");
-
-int Usage(const char* name)
-{
-  std::cout << "Usage: " << name << "\n"
-    << "  [" << argHelp << "]              show this help\n"
-    << "  [" << argTemplate << "[=W[xH]]]  print a template of dimensions width x height blocks to use for input\n"
-    << std::endl;
-  return EXIT_SUCCESS;
-}
-
 int main(int argc, char** argv)
 {
-  if(argc == 2) {
-    auto arg = std::string_view(argv[1]);
-
-    if(arg.starts_with(argHelp)) {
-      return Usage(argv[0]);
+  if(argc >= 2) {
+    auto args = ParseArgs(argc, argv);
+    if(args.help) {
+      return PrintUsage(std::cout, argv[0]);
     }
-
-    if(arg.starts_with(argTemplate)) {
-      arg.remove_prefix(argTemplate.size());
-      if(arg.starts_with('=')) {
-        arg.remove_prefix(1);
-        if(auto pos = arg.find('x'); pos < arg.size()) {
-          return PrintTemplate(
-            ParseValue<unsigned>(arg.substr(0, pos)),
-            ParseValue<unsigned>(arg.substr(pos + 1, std::string::npos)));
-        }
-        return PrintTemplate(ParseValue<unsigned>(arg));
-      }
-      return PrintTemplate(3, 3);
+    if(args.templ) {
+      return PrintTemplate(args.templ->width, args.templ->height);
     }
   }
 
@@ -99,7 +56,8 @@ int main(int argc, char** argv)
   Grid grid;
   iss >> grid;
 
-  std::cout << "\nParsed grid:\n\n" << grid << "\n";
+  std::cerr << "\nParsed grid:\n\n";
+  std::cout << grid << "\n";
 
-  return Usage(argv[0]);
+  return PrintUsage(std::cerr, argv[0]);
 }
