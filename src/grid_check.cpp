e2d31cc976f16ba765252053ba850f8e1172af4c
Marco Porsch <marco.porsch@posteo.de>
2024-06-11 21:28:10 +0200

    implement check purely using iterators

--- src/grid_check.cpp
+++ src/grid_check.cpp
@@ -19,46 +19,46 @@ bool IsSolved(Grid::container sorted)
 
 bool IsSolved(const Grid& grid)
 {
-  // check rows
-  for(size_t h = 0; h < grid.height(); ++h) {
-    auto first = &grid.at(h, 0);
-    auto last = &grid.at(h, grid.width());
-
-    if(!IsSolved(Grid::container(first, last))) {
-      return false;
+  { // check rows
+    auto firstFieldRow = make_custom_step_iterator(grid.begin(), grid.offsetOf<0>());
+    auto lastFieldRow = std::next(firstFieldRow, grid.height());
+    for(; firstFieldRow != lastFieldRow; ++firstFieldRow) {
+      auto firstField = firstFieldRow.base();
+      auto lastField = std::next(firstField, grid.width());
+      if(!IsSolved(Grid::container(firstField, lastField))) {
+        return false;
+      }
     }
   }
 
-  // check columns
-  for(size_t w = 0; w < grid.width(); ++w) {
-    auto first = make_custom_step_iterator(
-      std::next(grid.begin(), w),
-      grid.offsetOf<0>());
-    auto last = std::next(first, 1);
-
-    if(!IsSolved(Grid::container(first, last))) {
-      return false;
+  { // check columns
+    auto firstFieldCol = grid.begin();
+    auto lastFieldCol = std::next(firstFieldCol, grid.offsetOf<0>());
+    for(; firstFieldCol != lastFieldCol; ++firstFieldCol) {
+      auto firstField = make_custom_step_iterator(firstFieldCol, grid.offsetOf<0>());
+      auto lastField = std::next(firstField, 1);
+      if(!IsSolved(Grid::container(firstField, lastField))) {
+        return false;
+      }
     }
   }
 
   { // check blocks
-    auto it = grid.begin();
-    for(size_t h = 0; h < grid.height(); h += 3) {
-      for(size_t w = 0; w < grid.width(); w += 3) {
-        auto first = make_dance_dance_iterator(it,
+    auto firstBlockRow = make_custom_step_iterator(grid.begin(), grid.offsetOf<0>() * 3);
+    auto lastBlockRow = std::next(firstBlockRow, grid.height() / 3);
+    for(; firstBlockRow != lastBlockRow; ++firstBlockRow) {
+      auto firstBlockCol = make_custom_step_iterator(firstBlockRow.base(), 3);
+      auto lastBlockCol = std::next(firstBlockCol, grid.offsetOf<0>() / 3);
+      for(; firstBlockCol != lastBlockCol; ++firstBlockCol) {
+        auto firstField = make_dance_dance_iterator(firstBlockCol.base(),
            1,  1, grid.offsetOf<0>(),
           -1, -1, grid.offsetOf<0>(),
            1,  1, -2 - 2 * grid.offsetOf<0>()); // the last step goes back to the start
-        auto last = make_dance_dance_iterator(it);
-
-        if(!IsSolved(Grid::container(first, last))) {
+        auto lastField = make_dance_dance_iterator(firstField.base());
+        if(!IsSolved(Grid::container(firstField, lastField))) {
           return false;
         }
-
-        it += 3;
       }
-
-      it += 2 * grid.offsetOf<0>();
     }
   }
 
