ec381292533fd4616b7889774d2a00ce9e0c763f
Marco Porsch <marco.porsch@posteo.de>
2024-06-25 01:05:49 +0200

    use C++-23 ranges for Grid iteration

--- src/grid_check_impl.h
+++ src/grid_check_impl.h
@@ -1,60 +1,22 @@
 #pragma once
 
-#include "custom_step_iterator.h"
-#include "dance_dance_iterator.h"
 #include "grid.h"
-#include "preserve_iterator.h"
 
 #include <algorithm>
-#include <utility>
+#include <ranges>
 
-/// Ensure that dereferencing an iterator of the range returns
-/// the iterator itself (instead of a field reference) which can be used to
-/// create the next range
-template<typename FwdId>
-auto PreserveRange(std::pair<FwdId, FwdId> range)
+Grid::container ToGridContainer(std::ranges::viewable_range auto&& r)
 {
-  return std::make_pair(
-    make_preserve_iterator(range.first),
-    make_preserve_iterator(range.second));
-}
+  if constexpr(std::ranges::forward_range<decltype(r)>) {
+    return Grid::container(r.begin(), r.end());
+  }
 
-/// Iterate over grid vertically (over whole rows or fields in a column)
-/// @tparam  Step  Size to distinguish between stepping over fields or blocks
-/// @param  it  Starting point; must be in first row of \p grid
-template<size_t Step>
-auto RowRange(Grid::const_iterator it, const Grid& grid)
-{
-  auto first = make_custom_step_iterator(it, grid.offsetOf<0>() * Step);
-  auto last = std::next(first, grid.height() / Step);
-  return std::make_pair(first, last);
-}
-
-/// Iterate over grid horizontally (over whole columns or fields in a row)
-/// @tparam  Step  Size to distinguish between stepping over fields or blocks
-/// @param  it  Starting point; must be in first column of \p grid
-template<size_t Step>
-auto ColRange(Grid::const_iterator it, const Grid& grid)
-{
-  auto first = make_custom_step_iterator(it, Step);
-  auto last = std::next(first, grid.width() / Step);
-  return std::make_pair(first, last);
-}
-
-constexpr auto FieldRowRange = RowRange<1>;
-constexpr auto FieldColRange = ColRange<1>;
-constexpr auto BlockRowRange = RowRange<Grid::BlockSize>;
-constexpr auto BlockColRange = ColRange<Grid::BlockSize>;
-
-auto BlockFieldsRange(Grid::const_iterator it, const Grid& grid)
-{
-  // start at the top left field of a block
-  auto first = make_dance_dance_iterator(it, // +~~~~~~~+
-     1,  1, grid.offsetOf<0>(),              // | > > v |
-    -1, -1, grid.offsetOf<0>(),              // | v < < |
-     1,  1, -2 - 2 * grid.offsetOf<0>());    // | > > O |
-  auto last = make_dance_dance_iterator(it); // +~~~~~~~+
-  return std::make_pair(first, last); // the last step goes back to the start
+  Grid::container c;
+  c.reserve(Grid::BlockSize * Grid::BlockSize);
+  for(auto&&f : r) {
+    c.push_back(f);
+  }
+  return c;
 }
 
 // Sudoku check implemented as struct used as template parameter below so
@@ -80,36 +42,46 @@ struct Checker
 {
   const Grid& grid;
 
-  /// Variadic template to concatenate all_of iterations of layered ranges
-  template<typename FwdId, typename F, typename... Fs>
-  bool Check(
-    FwdId it,
-    F&& getRange,
-    Fs&&... getNextRanges) const
-  {
-    auto range = PreserveRange(getRange(it, grid));
-    return std::all_of(range.first, range.second,
-      [&](auto it) {
-        return Check(it, std::forward<Fs>(getNextRanges)...);
-      });
-  }
-
-  /// Variadic template terminator to create the final range which
-  /// determines a list of fields to call the actual Sudoku check with
-  template<typename FwdId, typename F>
-  bool Check(
-    FwdId it,
-    F&& getRange) const
-  {
-    auto range = getRange(it, grid);
-    return CheckFields()(Grid::container(range.first, range.second));
-  }
-
   operator bool() const
   {
     return true
-    && Check(grid.begin(), FieldRowRange, FieldColRange) // field rows
-    && Check(grid.begin(), FieldColRange, FieldRowRange) // field columns
-    && Check(grid.begin(), BlockRowRange, BlockColRange, BlockFieldsRange); // blocks by row and column
+    && std::ranges::all_of( // field rows
+      std::views::iota(0U, grid.height()),
+      [&](size_t row) {
+        return CheckFields()(ToGridContainer(
+          grid
+          | std::views::drop(row * grid.offsetOf<0>())
+          | std::views::take(grid.width())
+        ));
+      }
+    )
+    && std::ranges::all_of( // field columns
+      std::views::iota(0U, grid.width()),
+      [&](size_t col) {
+        return CheckFields()(ToGridContainer(
+          grid
+          | std::views::drop(col)
+          | std::views::stride(grid.offsetOf<0>())
+        ));
+      }
+    )
+    && std::ranges::all_of( // block rows
+      std::views::iota(0U, grid.height() / Grid::BlockSize),
+      [&](size_t row) {
+        return std::ranges::all_of( // block columns
+          std::views::iota(0U, grid.width() / Grid::BlockSize),
+          [&](size_t col) {
+            return CheckFields()(ToGridContainer(
+              grid // blocks by row and column
+              | std::views::drop((row * grid.offsetOf<0>() + col) * Grid::BlockSize)
+              | std::views::slide(Grid::BlockSize)
+              | std::views::stride(grid.offsetOf<0>())
+              | std::views::take(Grid::BlockSize)
+              | std::views::join
+            ));
+          }
+        );
+      }
+    );
   }
 };
