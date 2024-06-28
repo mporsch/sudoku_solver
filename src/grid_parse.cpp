9d25f72b8b38f29979b1092fed02ca4b31aa817e
Marco Porsch <marco.porsch@posteo.de>
2024-06-28 19:47:09 +0200

    parse grids while explicitly noting blocks

--- src/grid_parse.cpp
+++ src/grid_parse.cpp
@@ -7,6 +7,7 @@
 #include "lexy_ext/report_error.hpp"
 
 #include <algorithm>
+#include <numeric>
 #include <stdexcept>
 #include <string>
 #include <vector>
@@ -41,26 +42,39 @@ struct production
 
   struct fields
   {
-    // whole line with fields separated by either : or |
+    // fields separated by :
     static constexpr auto rule = [] {
-      auto terminator = dsl::terminator(dsl::eol);
       auto item = dsl::p<field>;
-      auto sep = dsl::trailing_sep(dsl::colon | dsl::vbar);
-      return terminator.list(item, sep);
+      auto sep = dsl::sep(dsl::colon);
+      return dsl::list(item, sep);
     }();
 
     // create list of fields
     static constexpr auto value = lexy::as_list<std::vector<Field>>;
   };
 
+  struct blocks
+  {
+    // whole line with blocks separated by |
+    static constexpr auto rule = [] {
+      auto terminator = dsl::terminator(dsl::eol);
+      auto item = dsl::p<fields>;
+      auto sep = dsl::trailing_sep(dsl::vbar);
+      return terminator.list(item, sep);
+    }();
+
+    // create list of blocks
+    static constexpr auto value = lexy::as_list<std::vector<std::vector<Field>>>;
+  };
+
   // ignore all whitespace encountered here and in sub-rules
   static constexpr auto whitespace = dsl::whitespace(dsl::ascii::blank);
 
-  // non-border field lines start with a |
-  static constexpr auto rule = dsl::opt(dsl::vbar >> dsl::p<fields>);
+  // non-border block/field lines start with a |
+  static constexpr auto rule = dsl::opt(dsl::vbar >> dsl::p<blocks>);
 
   // forward list of fields created by sub-rule or create empty list
-  static constexpr auto value = lexy::as_list<std::vector<Field>>;
+  static constexpr auto value = lexy::as_list<std::vector<std::vector<Field>>>;
 };
 
 } // namespace anonymous
@@ -74,17 +88,27 @@ std::istream& operator>>(std::istream& is, Grid& grid)
   while(std::getline(is, str)) {
     auto input = lexy::string_input(str);
     if(auto parsed = lexy::parse<production>(input, lexy_ext::report_error); parsed.has_value()) {
-      if(auto fields = parsed.value(); !fields.empty()) {
+      if(auto blocks = parsed.value(); !blocks.empty()) {
+        auto fields_width = std::accumulate(
+          begin(blocks), end(blocks),
+          0U,
+          [](size_t sum, auto&& block) {
+            return sum + block.size();
+          });
+
         if(auto current_width = grid.width()) {
-          if(current_width != fields.size()) {
+          if(current_width != fields_width) {
             throw std::invalid_argument("unequal width of grid lines");
           }
         }
 
         auto current_height = grid.height();
-        grid.resize(current_height + 1, fields.size());
+        grid.resize(current_height + 1, fields_width);
         auto current_row = grid[current_height];
-        (void)std::copy(begin(fields), end(fields), current_row.begin());
+        auto pos = current_row.begin();
+        for(auto&& block : blocks) {
+          pos = std::copy(begin(block), end(block), pos);
+        }
       }
     }
   }
