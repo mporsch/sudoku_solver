7e75b13539ca04a09892b34b8a5a55d33ee6bde2
Marco Porsch <marco.porsch@posteo.de>
2024-07-11 23:57:30 +0200

    solver documentation and cleanups

--- src/grid_solve.cpp
+++ src/grid_solve.cpp
@@ -56,12 +56,13 @@ struct AnnotateCandidates
   {
     auto givens = UniqueDigits(ToFields(range));
 
-    // the candidates of this group are all elements that are not given in it
+    // the candidates of this group are all elements that are not givens in it
     Field::Candidates groupCandidates;
+    groupCandidates.reserve(elements.size());
     (void)std::set_difference(
       begin(elements), end(elements),
       begin(givens), end(givens),
-      std::inserter(groupCandidates, begin(groupCandidates)));
+      std::back_inserter(groupCandidates));
 
     for(auto&& field : range) {
       if(field == Field()) {
@@ -112,9 +113,11 @@ struct CompareNumberOfCandidates
 
 Order GetOrder(Grid &grid)
 {
+  // get iterators to all fields
   auto order = Order(grid.size());
   std::iota(begin(order), end(order), grid.begin());
 
+  // filter out the iterators to already solved fields
   (void)order.erase(
     std::remove_if(
       begin(order), end(order),
@@ -125,12 +128,16 @@ Order GetOrder(Grid &grid)
     end(order)
   );
 
+  // sort iterators by number of unsolved field candidates
   std::sort(begin(order), end(order), CompareNumberOfCandidates{});
 
   return order;
 }
 
-IsSolved Solve(Grid &grid, Order::iterator curr, Order::iterator last)
+IsSolved Solve(
+  Grid &grid,
+  Order::iterator curr,
+  Order::iterator last)
 {
   // check if this branch is or can even be solved
   switch(auto isSolved = Check(grid)) {
@@ -143,12 +150,13 @@ IsSolved Solve(Grid &grid, Order::iterator curr, Order::iterator last)
       break;
   }
 
-  // the next field to check is after the one just written
-  auto next = std::next(curr);
-
-  // the field to write
+  // the unsolved field to attempt in this iteration
   auto&& field = *curr;
 
+  // the next unsolved field to check after this one
+  auto next = std::next(curr);
+
+  // iterate the unsolved field's candidates
   assert(field->candidates.has_value());
   for(auto&& candidate : *field->candidates) {
     // try a candidate element
@@ -172,10 +180,17 @@ IsSolved Solve(Grid &grid, Order::iterator curr, Order::iterator last)
 
 bool Solve(Grid grid)
 {
+  // annotate the unsolved fields with their candidates
   Annotate(grid);
 
+  // TODO more preprocessing to solve:
+  // - "Single" (or singleton, or lone number) – The only candidate in a cell
+  // - "Hidden single" – A candidate that appears with others, but only once in a given row, column or box
+
+  // get iterators to unsolved fields, sorted by number of candidates
   auto order = GetOrder(grid);
 
+  // start recursive solve iterations, then examine the outcome
   switch(Solve(grid, begin(order), end(order))) {
     case IsSolved::Yes:
       return true;
